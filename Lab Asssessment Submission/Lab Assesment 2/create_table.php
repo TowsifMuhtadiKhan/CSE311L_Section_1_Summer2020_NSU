1fdb188fc75661d69e7352629448db852c202f56
TowsifMuhtadiKhan <towsif.muhtadi@northsouth.edu>
2020-07-18 13:09:49 +0600

    Lab Assesment2

---
+++ Lab Asssessment Submission/Lab Assesment 2/create_table.php
@@ -0,0 +1,25 @@
+<?php
+
+$link = mysqli_connect('localhost','root','','VS_CODE_TEST');
+
+if($link == false)
+{
+    die("Error: Could not Connect." .mysqli_connect_error());
+}
+
+$sql = "CREATE TABLE test_table(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP)";
+if (mysqli_query($link,$sql))
+{
+ echo "Table Created";
+}
+ else
+ {
+   echo "Error : Could not able to create Table." .mysqli_error($link);
+ }
+ mysqli_close($link)
+
+?>
